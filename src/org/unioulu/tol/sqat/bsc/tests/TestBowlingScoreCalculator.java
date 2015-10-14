1d96b0276f3143786c48c2a5cbe316b09014bb4b
somename <someemail>
2015-10-14 15:14:26 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -9,7 +9,7 @@ import org.unioulu.tol.sqat.bsc.Frame;
 public class TestBowlingScoreCalculator {
 
 	@Test
-	public void testPlayerThrowsTwice() {
+	public void testPlayerThrowsFirstTwoSecondFour() {
 		Frame frame = new Frame(2,4);
 		assertEquals(2, frame.getFirstThrow());
 		assertEquals(4,frame.getSecondThrow());
